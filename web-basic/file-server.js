6e3e6fc8925b7b73a79333e727eccbb1c7da9720
Satoshi Yamamoto <satoshiyamamoto@me.com>
2015-12-28 11:30:38 +0900

    Add error handling when file not found.

--- web-basic/file-server.js
+++ web-basic/file-server.js
@@ -12,5 +12,9 @@ var server = http.createServer(function (req, res) {
   var path = join(root, url.pathname);
   var stream = fs.createReadStream(path);
   stream.pipe(res);
+  stream.on('error', function (err) {
+    res.statusCode = 500;
+    res.end('Internal Server Error');
+  });
 });
 server.listen(3000);
---
+++ web-basic/todos-web.js
@@ -0,0 +1,3 @@
+/**
+ * Created by satoshi on 12/28/15.
+ */
