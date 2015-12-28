5cd809b925374468ec5bb3fc1e8abdfb702b759e
Satoshi Yamamoto <satoshiyamamoto@me.com>
2015-12-28 11:02:36 +0900

    Replace code by Stream#pipe.

--- web-basic/file-server.js
+++ web-basic/file-server.js
@@ -11,11 +11,6 @@ var server = http.createServer(function (req, res) {
   var url = parse(req.url);
   var path = join(root, url.pathname);
   var stream = fs.createReadStream(path);
-  stream.on('data', function (chunk) {
-    res.write(chunk);
-  });
-  stream.on('end', function () {
-    res.end();
-  });
+  stream.pipe(res);
 });
 server.listen(3000);
