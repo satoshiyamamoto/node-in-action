876b2e84a93bc35b0e8422ecb9c06e8935d7fdff
Satoshi Yamamoto <satoshi_yamamoto@cyberagent.co.jp>
2016-01-03 15:49:35 +0900

    Improve deprecated codes.

--- connect/limit.js
+++ connect/limit.js
@@ -2,20 +2,34 @@
 var connect = require('connect');
 var bodyParser = require('body-parser');
 var getRawBody = require('raw-body');
-var typer = require('media-typer');
 
-var app = connect()
-  .use(function (req, res, next) {
+function limit(size) {
+  return function(req, res, next) {
     getRawBody(req, {
-      length: req.headers['content-length'],
-      limit: '32kb',
-      encoding: typer.parse(req.headers['content-type']).parameters.charset
+      limit: size
     }, function (err, string) {
       if (err) return next(err);
       req.text = string;
       next();
     });
-  })
+  };
+}
+
+function type(type, fn) {
+  return function (req, res, next) {
+    var ct = req.headers['content-type'] || '';
+    if (0 != ct.indexOf(type)) {
+      return next();
+    }
+    fn(req, res, next);
+  };
+}
+
+var app = connect()
+  .use(type('application/x-www-form-urlencoded', limit('64kb')))
+  .use(type('application/json', limit('32kb')))
+  .use(type('image', limit('2mb')))
+  .use(type('video', limit('300mb')))
   .use(bodyParser())
   .listen(3000);
 
