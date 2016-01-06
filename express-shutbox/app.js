7053e18b096d7160e083bbf30efb006e659db1d3
Satoshi Yamamoto <satoshi_yamamoto@cyberagent.co.jp>
2016-01-06 16:02:34 +0900

    Add pagenation middleware.

--- express-shutbox/app.js
+++ express-shutbox/app.js
@@ -6,12 +6,14 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
 var register = require('./routes/register');
 var login = require('./routes/login');
+var entries = require('./routes/entries');
 var user = require('./lib/middleware/user');
+var validate = require('./lib/middleware/validate');
+var page = require('./lib/middleware/page');
 var messages = require('./lib/messages');
+var Entry = require('./lib/entry');
 
 
 var app = express();
@@ -19,25 +21,37 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('secret', 'your secret here');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser('your secret here'));
-app.use(session());
+app.use(cookieParser(app.get('secret')));
+app.use(session({
+  secret: app.get('secret'),
+  resave: false,
+  saveUninitialized: true
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(user);
 app.use(messages);
 
-app.use('/', routes);
-app.use('/users', users);
 app.get('/register', register.form);
 app.post('/register', register.submit);
 app.get('/login', login.form);
 app.post('/login', login.submit);
 app.get('/logout', login.logout);
+app.get('/', page(Entry.count, 5), entries.list);
+app.get('/post', entries.form);
+app.post('/post',
+  validate.required('entry[title]'),
+  validate.lengthAbove('entry[title]', 4),
+  entries.submit
+);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
