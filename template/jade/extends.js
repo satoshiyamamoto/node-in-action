b070d2067c14cb1226cc3d118c121353fffc7e04
Satoshi Yamamoto <satoshi_yamamoto@cyberagent.co.jp>
2016-01-12 17:26:07 +0900

    Added mixins of Jade template.

--- template/jade/extends.js
+++ template/jade/extends.js
@@ -1,14 +1,24 @@
 // template/jade/extends.js
 var jade = require('jade');
 var fs = require('fs');
+var http = require('http');
 var pageFile = './views/page.jade';
 var page = fs.readFileSync(pageFile);
+
+var students = fs.readFileSync('../students.json', 'utf8');
+students = JSON.parse(students);
+var messages = [
+  'You have logged in successfully.',
+  'Welcome back!'
+];
 var context = {
-  messages: [
-    'You have logged in successfully.',
-    'Welcome back!'
-  ]
+  messages: messages,
+  students: students.students
 };
 
-var pageFn = jade.compile(page, {filename: pageFile});
-console.log(pageFn(context));
+http.createServer(function (req, res) {
+  var pageFn = jade.compile(page, {filename: pageFile});
+  res.setHeader('Content-Type', 'text/html');
+  res.end(pageFn(context));
+})
+.listen(3000);
